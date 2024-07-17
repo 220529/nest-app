91ea2f4d068d93ab9a6ee590f6097f4a722fc008
liukaixin <15713857919@163.com>
2024-07-17 15:21:01 +0800

    feat: 过滤器

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { HttpExceptionFilter } from '@/filter/http-exception.filter';
+import { TransformInterceptor } from '@/global/transform.interceptor';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 异常过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
+  // 响应拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   const options = new DocumentBuilder().build();
   const document = SwaggerModule.createDocument(app, options);
