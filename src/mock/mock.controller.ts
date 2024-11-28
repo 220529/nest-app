ff9ecbe863e4f78a2d2727fc300051ec1828a6c2
liukaixin <15713857919@163.com>
2024-11-28 15:39:48 +0800

    mock

--- src/mock/mock.controller.ts
+++ src/mock/mock.controller.ts
@@ -30,13 +30,13 @@ export class MockController {
     };
   }
 
-  @Get('pagination')
-  getPaginatedData(
+  @Get('customers')
+  getCustomersData(
     @Query('page') page: number = 1,
     @Query('size') size: number = 10,
   ) {
     // 总数据量
-    const totalItems = 23;
+    const totalItems = 12;
 
     // 使用 Mock.js 动态生成数据
     const items = Mock.mock({
@@ -59,7 +59,45 @@ export class MockController {
     const endIndex = Number(startIndex) + Number(size); // 计算当前页结束的数据索引
     const paginatedItems = items.slice(startIndex, endIndex); // 切片获取当前页的数据
 
-    console.log("page", page, size, startIndex, endIndex);
+    return {
+      page,
+      size,
+      totalItems,
+      totalPages: Math.ceil(totalItems / size),
+      data: paginatedItems, // 返回当前页的数据
+    };
+  }
+
+  @Get('dispatchs')
+  getDispatchsData(
+    @Query('page') page: number = 1,
+    @Query('size') size: number = 10,
+  ) {
+    // 总数据量
+    const totalItems = 21;
+
+    // 使用Mock.js动态生成数据
+    const items = Mock.mock({
+      [`data|${totalItems}`]: [
+        {
+          serialNumber: '@increment(1)',
+          roleType: '@pick(["渠道部经理", "其他角色", "项目经理", "技术主管"])',
+          originalPersonnel: '@cname',
+          departmentOfOriginalPersonnel: '@city()',
+          orderIssuingTime: '@datetime("yyyy-MM-dd HH:mm:ss")',
+          transferredPersonnel: '@cname',
+          departmentOfTransferredPersonnel: '@city()',
+          reasonForTransfer: '@pick(["更换人员", "业务调整", "项目变更"])',
+          operator: '@cname',
+          transferTime: '@datetime("yyyy-MM-dd HH:mm:ss")'
+        }
+      ]
+    }).data;
+
+    // 分页逻辑
+    const startIndex = (page - 1) * size; // 计算当前页开始的数据索引
+    const endIndex = Number(startIndex) + Number(size); // 计算当前页结束的数据索引
+    const paginatedItems = items.slice(startIndex, endIndex); // 切片获取当前页的数据
 
     return {
       page,
@@ -67,6 +105,60 @@ export class MockController {
       totalItems,
       totalPages: Math.ceil(totalItems / size),
       data: paginatedItems, // 返回当前页的数据
+    };
+  }
+
+  @Get('final-accounts')
+  getDispatchData() {
+    // 使用 Mock.js 动态生成数据
+    const dispatch = Mock.mock({
+      'list|6': [
+        {
+          'id': '@increment',
+          'date': '@datetime("yyyy-MM-dd HH:mm:ss")',
+          'name': '@cname',
+          'position': '@pick(["渠道部经理", "系统管理员A", "渠道专员"])'
+        }
+      ]
+    });
+
+    const customer = Mock.mock({
+      'name': '@cname',
+      'dispatchPhone': /1[3-9]\d{9}/,
+      'signingPhone': /1[3-9]\d{9}/,
+      'decorationAddress': '@county(true) @city(true) @province(true) @natural(1, 10) @string("number", 3) 号 @company',
+      'houseArea': '@float(60, 150, 2, 2)',
+      'houseStatus': '@pick(["新房", "老房"]) @pick(["有电梯", "无电梯"]) @pick(["半包", "全包"]) @pick(["出租", "自住"])',
+      'houseType': '@integer(1, 5)室@integer(1, 3)厅@integer(1, 2)厨@integer(1, 3)卫@integer(0, 2)阳台'
+    });
+
+    // 模拟订单流程数据
+    let orderProcess = Mock.mock({
+      'orderProcess|7': [
+        {
+          'title': function () {
+            const names = ['创建订单', '转订单', '确认签单', '锁定订单', '开工申请通过', '工程排期', '竣工'];
+            return names[Mock.Random.integer(0, 6)];
+          },
+          'time': Mock.Random.datetime('yyyy-MM-dd HH:mm:ss'),
+          // 'status': function () {
+          //   const statusOptions = ['done', 'doing', 'undone'];
+          //   return statusOptions[Mock.Random.integer(0, 2)];
+          // },
+          'description': '@pick(["已完成", "进行中", "未开始"])'
+        }
+      ]
+    }).orderProcess;
+
+    orderProcess = orderProcess.map((item, index) => {
+      return {
+        id: index + 1,
+        ...item
+      }
+    })
+
+    return {
+      orderProcess: orderProcess, customer, dispatchInformation: dispatch.list
     };
   }
 
