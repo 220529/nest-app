aa92a08c891dc0c598fd913bdf6dc1671e286bff
liukaixin <15713857919@163.com>
2025-06-18 09:17:25 +0800

    test: rbac

--- src/work/work.controller.ts
+++ src/work/work.controller.ts
@@ -9,17 +9,23 @@ import {
   Query,
   Request,
   BadRequestException,
+  UseGuards,
 } from "@nestjs/common";
 import { PaginationDto } from "@/common/dto/pagination.dto";
 import { WorkService } from "./work.service";
 import { CreateWorkDto } from "./dto/create-work.dto";
 import { UpdateWorkDto } from "./dto/update-work.dto";
+import { Role } from "../enums/role.enum";
+import { Roles } from "../decorators/roles.decorator";
+import { RolesGuard } from "@/guards/roles.guard";
 
 @Controller("work")
+@UseGuards(RolesGuard)
 export class WorkController {
   constructor(private readonly worksService: WorkService) {}
 
   @Post()
+  @Roles(Role.Admin)
   create(@Request() req, @Body() createWorkDto: CreateWorkDto) {
     return this.worksService.create(req.user.id, createWorkDto);
   }
